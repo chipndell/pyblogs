da4d9ba51a206a5881838212c5d8a0098c632beb
Manan Bhavsar <mananbh9@live.com>
2023-06-30 05:17:24 -0400

    changed name to pyblogs

--- pyblogs/asgi.py
+++ pyblogs/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for socialpost project.
+ASGI config for pyblogs project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialpost.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyblogs.settings")
 
 application = get_asgi_application()
--- pyblogs/wsgi.py
+++ pyblogs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for socialpost project.
+WSGI config for pyblogs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialpost.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyblogs.settings")
 
 application = get_wsgi_application()
